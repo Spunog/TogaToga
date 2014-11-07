c17195058fd6f9b32e5740cb45fcd32e019c53d9
Spunog <Spunog@gmail.com>
2014-11-07 00:09:33 +0000

    Update style of movie list on detail page

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -18,8 +18,8 @@ class MoviesController < ApplicationController
   end
 
   def rt
-    rt_movie_responce = HTTParty.get('http://localhost:3000/home/apiRTMovieTest.json')
-    rt_movie_review_responce = HTTParty.get('http://localhost:3000/home/apiRTTest.json')
+    rt_movie_responce = HTTParty.get('http://www.togatoga.me/home/apiRTMovieTest.json')
+    rt_movie_review_responce = HTTParty.get('http://www.togatoga.me/home/apiRTTest.json')
     @movie = rt_movie_responce
     @reviews = rt_movie_review_responce
   end
